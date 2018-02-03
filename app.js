d7387c2973a9b0c6b45af7fc4595ed517cf10d0d
Liam Owen <liam@polygami.co.uk>
2018-02-03 08:34:14 +0100

    Updated app.js to initialise Express, set EJS as the default view engine and created 2 simple routes for the home and about pages.

---
+++ app.js
@@ -0,0 +1,14 @@
+// These 2 lines are used to initialise express
+var express = require("express");
+var app = express();
+
+// Tell Express what the default file extension will be for files that will be rendered
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+	res.render("home");
+});
+
+app.get("/about", function(req, res) {
+	res.render("about");
+});
